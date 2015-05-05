106969ae111374000da17b8174dc93534646a3b5
RaviWoods <rbw14@eews104a-005.ic.ac.uk>
2015-05-05 16:17:04 +0100

    redtint

---
+++ edge_detect2/Catapult/red_tint.v1/cycle_set.tcl
@@ -0,0 +1,19 @@
+
+# Loop constraints
+directive set /red_tint/core/main CSTEPS_FROM {{. == 2}}
+
+# IO operation constraints
+directive set /red_tint/core/main/io_read(mouse_xy:rsc.d) CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/io_read(video_in:rsc.d)#1 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/io_write(video_out:rsc.d) CSTEPS_FROM {{.. == 1}}
+
+# Real operation constraints
+directive set /red_tint/core/main/acc#2 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/acc#5 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/acc#4 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/acc#7 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/acc#6 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos:acc#1 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos:mux#1 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos#1:acc#1 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos#1:mux#1 CSTEPS_FROM {{.. == 1}}
---
+++ edge_detect2/Catapult/red_tint.v4/cycle_set.tcl
@@ -0,0 +1,19 @@
+
+# Loop constraints
+directive set /red_tint/core/main CSTEPS_FROM {{. == 2}}
+
+# IO operation constraints
+directive set /red_tint/core/main/io_read(mouse_xy:rsc.d) CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/io_read(video_in:rsc.d)#1 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/io_write(video_out:rsc.d) CSTEPS_FROM {{.. == 1}}
+
+# Real operation constraints
+directive set /red_tint/core/main/acc CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/acc#4 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos:else:else:acc CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/acc#5 CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos#1:else:else:acc CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos:else:mux CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos:and CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos#1:else:mux CSTEPS_FROM {{.. == 1}}
+directive set /red_tint/core/main/makepos#1:and CSTEPS_FROM {{.. == 1}}
